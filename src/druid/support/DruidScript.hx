22109ca8f243685aa72ea4c7746027eb1f4f9994
AsakuraMizu <0xWATERx0@gmail.com>
2020-08-17 19:31:35 +0800

    better DruidScript

--- src/druid/support/DruidScript.hx
+++ src/druid/support/DruidScript.hx
@@ -5,9 +5,10 @@ import defold.types.Message;
 import defold.types.Url;
 import defold.support.GuiScript;
 import defold.support.ScriptOnInputAction;
+import druid.types.DruidStyle;
 
 class DruidScript<T:{}> extends GuiScript<T> {
-    private var druid:Druid<T>;
+    private var druid_instance:Druid<T>;
 
     /**
         Add component
@@ -15,26 +16,40 @@ class DruidScript<T:{}> extends GuiScript<T> {
         @param component Component
     **/
     private function add(component:Component<T>):Void
-        druid.add(component);
+        druid_instance.add(component);
 
-    override function init(self:T) {
-        super.init(self);
-        druid = Manager.create(self);
-    }
+    /**
+        Set druid style
 
-    override function final_(self:T) {
-        druid.final_();
-    }
+        Invoke `set_style` on every component.
 
-    override function on_input(self:T, action_id:Hash, action:ScriptOnInputAction):Bool {
-        return druid.on_input(action_id, action);
-    }
+        @param druid_style druid style
+    **/
+    private function set_style(?style: DruidStyle):Void
+        druid_instance.set_style(style);
 
-    override function on_message<TMessage>(self:T, message_id:Message<TMessage>, message:TMessage, sender:Url) {
-        druid.on_message(message_id, message,sender);
-    }
+    /**
+        Remove component from druid instance
 
-    override function update(self:T, dt:Float) {
-        druid.update(dt);
-    }
+        Component `on_remove` function will be invoked, if exist.
+
+        @param component Component instance
+    **/
+    public function remove(component: Component<T>):Void
+        druid_instance.remove(component);
+
+    override function init(self:T):Void
+        druid_instance = Manager.create(self);
+
+    override function final_(self:T):Void
+        druid_instance.final_();
+
+    override function on_input(self:T, action_id:Hash, action:ScriptOnInputAction):Bool
+        return druid_instance.on_input(action_id, action);
+
+    override function on_message<TMessage>(self:T, message_id:Message<TMessage>, message:TMessage, sender:Url):Void
+        druid_instance.on_message(message_id, message,sender);
+
+    override function update(self:T, dt:Float):Void
+        druid_instance.update(dt);
 }
